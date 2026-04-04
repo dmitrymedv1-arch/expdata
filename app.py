9ab56c900c88c40c49a28f39051bbe7bb0a58574
dmitrymedv1-arch <dmitrymedv1@gmail.com>
2026-04-04 12:58:21 +0500

    app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Patch
 from matplotlib.lines import Line2D
 import random
 
